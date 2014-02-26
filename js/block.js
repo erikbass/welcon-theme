8f3180aedc6ba313debfba19d1c47089fd24b5e3
Erik <erikguimaraesdasilva@gmail.com>
2014-02-26 18:04:12 -0400

    Iniciando o block com os elementos posicionados (fix para IE)

--- js/block.js
+++ js/block.js
@@ -1,4 +1,15 @@
 jQuery(document).ready(function() { 	
+	/* iniciando posicionado */
+		jQuery(".block li:first-child .blocks").animate({height : 290, width : 363, left : 0}, 100);
+		jQuery(".block li:first-child .blocks .imgBlock").animate({height : 241, width : 363}, 100);
+
+		jQuery(".block li:nth-child(2) .blocks").animate({height : 140,width : 735, top : 0, left: 373}, 100);
+		jQuery(".block li:nth-child(2) .blocks .imgBlock").animate({height : 140, width : 500}, 100);
+		
+		jQuery(".block li:nth-child(3) .blocks").animate({height : 140, opacity : 1, width : 735, top : 150, left : 373}, 100);
+		jQuery(".block li:nth-child(3) .blocks .imgBlock").animate({height : 140, width : 500}, 100);
+	/* */
+
 	if (jQuery(".block li:nth-child(4)").length < 1){
 		jQuery(".prev").hide();
 		jQuery(".next").hide();
@@ -7,26 +18,25 @@ jQuery(document).ready(function() {
 	jQuery('.prev').click(function() {				 
 		var elementoPai = jQuery(this).parent().siblings('ul');
 		//Move o item
-		jQuery(elementoPai).animate({'left' : 0}, 'fast', function(){	
+		jQuery(elementoPai).animate({'left' : 0}, 100, function(){	
 			
 			// Block 03 - some
-				jQuery(elementoPai).find('li:nth-child(3) .blocks').animate({left : -363, opacity : 0}, "fast");
-				jQuery(elementoPai).find('li:nth-child(3) .blocks').animate({top : 290}, "fast");
-				jQuery(elementoPai).find('li:nth-child(3) .blocks').animate({left : 373}, "fast");
-
+				jQuery(elementoPai).find('li:nth-child(3) .blocks').animate({left : -363, opacity : 0}, 100);
+				jQuery(elementoPai).find('li:nth-child(3) .blocks').animate({top : 290}, 100);
+				jQuery(elementoPai).find('li:nth-child(3) .blocks').animate({left : 373}, 100);
 			// Block 02 -> Block 03
-				jQuery(elementoPai).find('li:nth-child(2) .blocks').animate({height : 140, opacity : 1}, "fast").animate({width : 735, top : 150, left : 373}, "fast");
-				jQuery(elementoPai).find('li:nth-child(2) .blocks .imgBlock').animate({height : 140}, "fast").animate({width : 500}, "fast");
+				jQuery(elementoPai).find('li:nth-child(2) .blocks').animate({height : 140, opacity : 1}, 100).animate({width : 735, top : 150, left : 373}, 100);
+				jQuery(elementoPai).find('li:nth-child(2) .blocks .imgBlock').animate({height : 140}, 100).animate({width : 500}, 100);
 			// Block 01 -> Block 02
-				jQuery(elementoPai).find('li:first .blocks').animate({height: 140, width: 735, top: 0, left: 373},"fast");
-				jQuery(elementoPai).find('li:first .blocks .imgBlock').animate({height : 140}, "fast").animate({width : 500}, "fast");
+				jQuery(elementoPai).find('li:first-child .blocks').animate({height: 140, width: 735, top: 0, left: 373},100);
+				jQuery(elementoPai).find('li:first-child .blocks .imgBlock').animate({height : 140}, 100).animate({width : 500}, 100);
 			// Block 04 -> Block 01
-				jQuery(elementoPai).find('li:nth-child(4) .blocks').animate({height : 290, width : 363, left : 0, top:0, opacity:1}, "fast");
-				jQuery(elementoPai).find('li:nth-child(4) .blocks .imgBlock').animate({height : 241}, "fast").animate({width : 363}, "fast");
+				jQuery(elementoPai).find('li:nth-child(4) .blocks').animate({height : 290, width : 363, left : 0, top:0, opacity:1}, 100);
+				jQuery(elementoPai).find('li:nth-child(4) .blocks .imgBlock').animate({height : 241}, 100).animate({width : 363}, 100);
 			//		
 		
 			//Move o último item e o coloca como o primeiro
-				jQuery(elementoPai).find('li:first').before(jQuery(elementoPai).find('li:last'));
+				jQuery(elementoPai).find('li:first-child').before(jQuery(elementoPai).find('li:last'));
 		});		
 		//Cancela o comportamento do click
 		return false;				 
@@ -36,29 +46,29 @@ jQuery(document).ready(function() {
 	jQuery('.next').click(function() {				 
 		var elementoPai = jQuery(this).parent().siblings('ul');
 		//Move o item
-		jQuery(elementoPai).animate({'left' : 0}, "fast", function () {	
+		jQuery(elementoPai).animate({'left' : 0}, 100, function () {	
 			
 			// Block 01 - some
-				jQuery(elementoPai).find('li:first .blocks').animate({left : -363, opacity : 0}, "fast");
-				jQuery(elementoPai).find('li:first .blocks').animate({top : 290}, "fast");
-				jQuery(elementoPai).find('li:first .blocks').animate({left : 373}, "fast");
+				jQuery(elementoPai).find('li:first-child .blocks').animate({left : -363, opacity : 0}, 100);
+				jQuery(elementoPai).find('li:first-child .blocks').animate({top : 290}, 100);
+				jQuery(elementoPai).find('li:first-child .blocks').animate({left : 373}, 100);
 
 			// Block 02 -> Block 01
-				jQuery(elementoPai).find('li:nth-child(2) .blocks').animate({height : 290, width : 363, left : 0}, "fast");
-				jQuery(elementoPai).find('li:nth-child(2) .blocks .imgBlock').animate({height : 241}, "fast").animate({width : 363}, "fast");
+				jQuery(elementoPai).find('li:nth-child(2) .blocks').animate({height : 290, width : 363, left : 0}, 100);
+				jQuery(elementoPai).find('li:nth-child(2) .blocks .imgBlock').animate({height : 241, width : 363}, 100);
 			// Block 03 -> Block 02
-				jQuery(elementoPai).find('li:nth-child(3) .blocks').animate({height : 140,width : 735, top : 0},"fast");
-				jQuery(elementoPai).find('li:nth-child(3) .blocks .imgBlock').animate({height : 140}, "fast").animate({width : 500}, "fast");
+				jQuery(elementoPai).find('li:nth-child(3) .blocks').animate({height : 140,width : 735, top : 0},100);
+				jQuery(elementoPai).find('li:nth-child(3) .blocks .imgBlock').animate({height : 140, width : 500}, 100);
 			// Block 04 -> Block 03
-				jQuery(elementoPai).find('li:nth-child(4) .blocks').animate({height : 140, opacity : 1}, "fast").animate({width : 735, top : 150, left : 373}, "fast");
-				jQuery(elementoPai).find('li:nth-child(4) .blocks .imgBlock').animate({height : 140}, "fast").animate({width : 500}, "fast");
+				jQuery(elementoPai).find('li:nth-child(4) .blocks').animate({height : 140, opacity : 1, width : 735, top : 150, left : 373}, 100);
+				jQuery(elementoPai).find('li:nth-child(4) .blocks .imgBlock').animate({height : 140, width : 500}, 100);
 			//		
 		
 			//Move o último item e o coloca como o primeiro
-				jQuery(elementoPai).find('li:last').after(jQuery(elementoPai).find('li:first'));
+				jQuery(elementoPai).find('li:last').after(jQuery(elementoPai).find('li:first-child'));
 		});
 
 
 		return false;				 
 	});
-});
+	});
